923a682de2518c1bbff47eedc14ce1ea287902f6
Niteesh Valaparla <valaparlaniteesh@gmail.com>
2026-06-09 21:48:38 +0530

    Add Terraform infrastructure configuration
    Added AWS provider and security group configuration for Docker Swarm.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,92 @@
-# Terraform configuration
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "docker_sg" {
+  name        = "docker-sg"
+  description = "Docker Swarm Security Group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["49.43.231.185/32"]
+  }
+
+  ingress {
+    from_port   = 2377
+    to_port     = 2377
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8500
+    to_port     = 8500
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8600
+    to_port     = 8600
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "node_a" {
+  ami           = var.ami_id
+  instance_type = "t3.micro"
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.docker_sg.id
+  ]
+
+  tags = {
+    Name = "Node-A"
+  }
+}
+
+resource "aws_instance" "node_b" {
+  ami           = var.ami_id
+  instance_type = "t3.micro"
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.docker_sg.id
+  ]
+
+  tags = {
+    Name = "Node-B"
+  }
+}
